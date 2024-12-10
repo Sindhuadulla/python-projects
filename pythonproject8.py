dbcfbd4411d2205936ae6f2044ae102c3b7a79cb
Sindhuadulla <137965504+Sindhuadulla@users.noreply.github.com>
2024-12-10 11:39:42 +0530

    Add files via upload

---
+++ pythonproject8.py
@@ -0,0 +1,24 @@
+from tkinter import * 
+
+root=Tk()
+
+root.title("Welcome ")
+
+root.geometry("350x200")
+
+
+lbl=Label(root,text="Are you a student")
+lbl.grid() 
+
+txt = Entry(root, width=10)
+txt.grid(column =1, row =0)
+
+def clicked():
+    res = "You wrote " + txt.get()
+    lbl.configure(text=res) 
+
+btn = Button(root, text = "Click me" ,
+             fg = "red", command=clicked)
+btn.grid(column=2, row=0)
+
+root.mainloop() 
